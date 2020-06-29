7f97b4dda83a5b5ae82f5b55ab88833b9524c665
Dazha Brown <dajahbrown@hotmail.com>
2020-06-29 05:14:06 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,8 +10,8 @@ birthday_kids = {
 #   "Amanda" => 27
 # }
 
-  def happy_birthday(birthday_kids)
-    birthday_kids.each do |name, age|
-      puts "Happy Birthday #{name}! You are now #{age} years old!"
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
